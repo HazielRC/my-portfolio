f0037ceafaecc5eac69f3dd4bcbd2b8ffc58abf0
HazielRC <haziel.contacto@gmail.com>
2023-07-31 20:51:34 -0600

    Portfolio and dark mode

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import '../stylesheets/portfolio/Portfolio.css'
+import '../stylesheets/portfolio/Portfolio-Mobile.css'
+
+const Portfolio = () => {
+  return (
+    <div className="container-portfolio" id="portfolio">
+
+      <h1 className="portfolio__title primary">Portfolio</h1>
+      <div className="portfolio__projects">
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Nothing</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Nothing</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Nothing</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Nothing</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Nothing</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Example</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Example</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Example</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Example</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Example</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Example</h1></a>
+        </div>
+        <div className="card">
+          <a href="#null" className="project__name"><h1>Example</h1></a>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Portfolio;
