43d5671ccd44d522b6469eb4e1ebaa58b643b12b
HazielRC <haziel.contacto@gmail.com>
2023-08-03 02:36:11 -0600

    Portfolio modifications

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,43 +8,6 @@ const Portfolio = () => {
 
       <h1 className="portfolio__title primary">Portfolio</h1>
       <div className="portfolio__projects">
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Nothing</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Nothing</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Nothing</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Nothing</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Nothing</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Example</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Example</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Example</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Example</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Example</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Example</h1></a>
-        </div>
-        <div className="card">
-          <a href="#null" className="project__name"><h1>Example</h1></a>
-        </div>
-
       </div>
 
     </div>
